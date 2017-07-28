6f0ccea62d324d2f9d9f3e48e146f0a2a0adce91
fang-ren <ren.fang.ren@gmail.com>
2017-07-28 14:22:12 -0700

    n.a.

---
+++ Leetcode/medium/40. Combination Sum II_DFS.py
@@ -0,0 +1,12 @@
+"""
+author: Fang Ren (SSRL)
+
+7/28/2017
+"""
+class Solution(object):
+    def combinationSum2(self, candidates, target):
+        """
+        :type candidates: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
