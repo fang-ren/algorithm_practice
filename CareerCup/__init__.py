8228def7eb8a08dcad44318d7d92b34c0f8e1336
fang-ren <ren.fang.ren@gmail.com>
2017-08-22 13:47:32 -0700

    Career cup

---
+++ CareerCup/__init__.py
@@ -0,0 +1,5 @@
+"""
+author: Fang Ren (SSRL)
+
+8/22/2017
+"""
---
+++ MIT_intro_algorithm/ps1/ps1.py
@@ -0,0 +1,17 @@
+"""
+author: Fang Ren (SSRL)
+
+8/8/2017
+"""
+
+"""
+Exercise 1-1 (2.3-6) P37.
+"""
+
+def insertion_sort_binary_search(A):
+
+
+"""
+Exercise 1-2
+
+"""
